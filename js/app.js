5a6ea2d57851b6c1758ac9ddeb0cc12c50753b52
MalihaKabir <malihakabir01@gmail.com>
2020-08-01 11:46:22 +0600

    initiate

---
+++ js/app.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // first of all, to make the game, deciding grid size
+  const widthOfGrid = 10; // 10 squares
+  const heightOfGrid = 20; // 20 squares
+})
